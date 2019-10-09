abbf3818ba9a148859e3efbb10c431f057ab8298
Miqueias Severo <miq.severo@gmail.com>
2019-10-08 22:13:28 -0300

    appMobile

---
+++ AppBuscaMecanicaMobile/src/app/login/login.page.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor(private qrScanner: QRScanner) { }
+
+  PegaUsuario(){
+    this.qrScanner.prepare()
+  .then((status: QRScannerStatus) => {
+     if (status.authorized) {
+       
+       let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+         console.log('Scanned something', text);
+
+          alert(text);
+         this.qrScanner.hide(); // hide camera preview
+         scanSub.unsubscribe(); // stop scanning
+       });
+
+     } else if (status.denied) {
+      
+     } else {
+       
+     }
+  })
+  .catch((e: any) => console.log('Error is', e));
+
+  }
+  
+  
+  
+  ngOnInit() {
+  }
+  
+}
