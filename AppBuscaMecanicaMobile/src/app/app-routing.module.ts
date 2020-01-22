f3a479a8903c47195278e3fb39669d04b231d520
MIQUEIAS SEVERO DA SILVA <miqueias.ssilva@senacsp.edu.br>
2020-01-21 22:05:03 -0300

    pastas atualizadas

--- AppBuscaMecanicaMobile/src/app/app-routing.module.ts
+++ AppBuscaMecanicaMobile/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
  
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: '', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'cadastrar', loadChildren: './cadastrar/cadastrar.module#CadastrarPageModule' }
 
  
