c930dbde820f825e4433c5e9cf7030028ff6a644
MIQUEIAS SEVERO DA SILVA <miqueias.ssilva@senacsp.edu.br>
2020-01-23 22:13:55 -0300

    CadastroVeiculo

--- AppBuscaMecanicaMobile/src/app/cadastrar/cadastrar.page.ts
+++ AppBuscaMecanicaMobile/src/app/cadastrar/cadastrar.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-cadastrar',
   templateUrl: './cadastrar.page.html',
@@ -8,24 +9,47 @@ import { NavController } from '@ionic/angular';
 export class CadastrarPage implements OnInit {
 
     etapaCadastro: number = 1;
+    formCadastroCliente = new FormGroup({
+      placaCadastro: new FormControl('', Validators.required),
+      nomeCadastro: new FormControl('', Validators.required),
+      sNomeCadastro: new FormControl('', Validators.required),
+      senhaCadastro: new FormControl('', Validators.required),
+      confirmaSenhaCadastro: new FormControl('', Validators.required),
+      cepCadastro: new FormControl('', Validators.required),
+      ruaCadastro: new FormControl('', Validators.required),
+      bairroCadastro: new FormControl('', Validators.required),
+      cidadeCadastro: new FormControl('', Validators.required),
+      estadoCadastro: new FormControl('', Validators.required),
+      numeroCadastro: new FormControl('', Validators.required),              
+      marcaCadastro: new FormControl('', Validators.required),
+      modeloCadastro: new FormControl('', Validators.required),
+      anoFabCadastro: new FormControl('', Validators.required),
+      anoModCadastro: new FormControl('', Validators.required)
+     
+    });
 
   constructor(public navCrtl:NavController) { }
 
   ngOnInit() {
   }
+  
+  mostarDadosCad(){
+    let form = this.formCadastroCliente.value;
+    console.log(form);
+  }
+   
 
-  recebeDados(etapaCadastro: number) {    
-    if (etapaCadastro === 3) {
-        
-    }
 
+  recebeDados() {    
     this.etapaCadastro++;
   }
   
-  voltaDadosAnterior(etapaCadastro: number) {    
-    
-
+  voltaDadosAnterior() {    
     this.etapaCadastro--;
+  }
+
+  vaiParaEtapa(etapa: number) {
+    this.etapaCadastro = etapa;
   }
 
   ChamaCadastro() {
